4a4de45d18b08675a347f19f0a0468a5aca3e6a8
Shelley Valdés <35204421+omtatelo@users.noreply.github.com>
2019-05-22 01:06:58 -0500

    update a sitio de resultados
    banner fluído cambia de proporciín, se empiezan a añadir items. Falta CSS

--- redschool.js
+++ redschool.js
@@ -3,7 +3,7 @@
 
 $(window).scroll(function() {
 
-    if ($(window).scrollTop() >= 50) {
+    if ($(window).scrollTop() >= 1) {
         $('nav').css('background', '#383838');
         $('nav').css('box-shadow','0 1px 2px 0 rgba(0,0,0,0.5)');
     } else {
